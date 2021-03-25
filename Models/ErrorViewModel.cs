ea3d1339a895512822419d12d4a17bd34d8ded64
Oscar090132 <oscar.garcia@live.com>
2021-03-25 13:53:28 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ejecuciones.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
